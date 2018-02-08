e5d460bc3475fdca2b9d08b30227ab6982465108
Luiz <lfernando-silva@hotmail.com>
2018-02-08 08:52:23 -0200

    Pass json file instead path, findById retrieving also raw data from firestore

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const id = 'wGr1piJGeswQtEIuNgbh'
 const collection = 'users'
+const serviceAccountKey = require('./configs/firestore-test-194518-firebase-adminsdk-agupj-367d8a5018.json')
 
-const lib = require('./lib/index')('./configs/firestore-test-194518-firebase-adminsdk-agupj-367d8a5018.json')
+//@todo take out the path, client passes the JSON as param
+const lib = require('./lib/index')(serviceAccountKey)
 
 return lib
-    .find({collection,id})
+    .findById({collection,id}, {rawData: true})
     .then(result => {
         return console.log(result)
     }).catch(err => {
